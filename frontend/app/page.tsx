6397c509932f8d1b0c2bf222f6525908b7b0b975
MikaelaAdolfsson <korpoga13@gmail.com>
2025-03-10 13:20:26 +0100

    Send button completed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { SendHorizontal } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <Button size={"icon"} className="rounded-full" variant={"liu"}>
-        Hello
+    <div className="flex flex-col justify-center items-center grow">
+      <Button
+        size={"icon"}
+        className="rounded-full flex justify-center items-center"
+        variant={"liu"}
+      >
+        <SendHorizontal className="size-6" />
       </Button>
     </div>
   );
+}
+
+// SEND MESSAGE BUTTON
+{
+  /* <Button
+  size={"icon"}
+  className="rounded-full flex justify-center items-center"
+  variant={"liu"}
+>
+  <SendHorizontal className="size-6" />
+</Button>; */
 }
