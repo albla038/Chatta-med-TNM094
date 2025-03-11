b4cbdffb6cc26c3315ebdbb6c83a61d6759fb5ea
MikaelaAdolfsson <korpoga13@gmail.com>
2025-03-11 09:01:15 +0100

    Minor adjustments

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,35 +1,14 @@
-// import { Button } from "@/components/ui/button";
 // import { Square } from "lucide-react";
 
-import { TextField } from "@/components/text-field";
+import { ChatInput } from "@/components/chat-input";
 
 export default function Home() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center grow">
-        <TextField></TextField>
+      <div className="flex flex-col justify-end items-center grow">
+        <div>text-field</div>
+        <ChatInput></ChatInput>
       </div>
     </>
   );
-}
-
-// SEND MESSAGE BUTTON
-
-/* <Button
-  size={"icon"}
-  className="rounded-full flex justify-center items-center"
-  variant={"liu"}
->
-  <SendHorizontal className="size-6" />
-</Button>; */
-
-// STOP BUTTON
-{
-  /* <Button
-size={"icon"}
-className="rounded-full flex justify-center items-center"
-variant={"outline"}
->
-<Square className="size-6 bg-transparent fill-input text-transparent" />
-</Button> */
 }
---
+++ frontend/components/chat-input.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Textarea } from "./ui/textarea";
+import { SendHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function ChatInput() {
+  return (
+    <div className="w-full p-8">
+      <div className="w-full relative">
+        <Button
+          size={"icon"}
+          className="absolute right-3 top-3 rounded-full flex justify-center items-center"
+          variant={"liu"}
+        >
+          <SendHorizontal className="size-6" />
+        </Button>
+        <Textarea
+          placeholder="Ställ din fråga ..."
+          className="shadow-xs pr-14 resize-none field-sizing-content h-fit  min-h-24"
+        ></Textarea>
+      </div>
+    </div>
+  );
+}
