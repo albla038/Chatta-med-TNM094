dca434d382660d90ba35b20188e5dbefc7f3cbc1
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-03-12 14:49:36 +0100

    PDF upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, status
-from .services import handle_question
+from fastapi import FastAPI, status, UploadFile
+from .services import handle_question, handle_upload_pdf
 from .models import QuestionReqBody
 from .vector_db import vector_db
 from .vector_db import retriever
@@ -28,7 +28,23 @@ async def create_vector(query: str):
   result = await vector_db.aadd_documents([document], ids=[str(uuid4())])
   return {"status": "ok", "message": "Vector created successfully", "data": result}
 
-@app.get("/vector",)
+@app.get("/vector")
 async def find_similar_vectors(query: str):
-  result = await find_vectors_with_query(query, k=5, threshold=0.9)
+  result = await find_vectors_with_query(query, k=5, threshold=0.5)
   return {"status": "ok", "message": "Search successful", "data": result}
+
+# @app.post("/upload/text")
+# async def upload_text():
+#   pass
+
+@app.post("/upload/pdf")
+async def upload_pdf(file: UploadFile):
+  data = await handle_upload_pdf(file)
+
+  return {
+    "status": "ok",
+    "message": "File uploaded successfully",
+    "filename": file.filename,
+    "content_type": file.content_type,
+    "data": data
+  }
--- backend/app/services.py
+++ backend/app/services.py
@@ -1,5 +1,13 @@
 from .models import QuestionReqBody
 from .llm import call_model
+from fastapi import HTTPException, UploadFile
+from langchain_community.document_loaders import PyPDFLoader 
+import os
+
+# Define file directory and create it if it doesn't exist
+# This directory will be used to store uploaded PDF files
+UPLOAD_DIR = "uploads/pdf"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 async def handle_question(req_body: QuestionReqBody): 
   # Retrive relevant text/inputs from vector database...
@@ -8,3 +16,20 @@ async def handle_question(req_body: QuestionReqBody):
     "content": model_reponse.content,
     "metadata": model_reponse.response_metadata
   }
+
+async def handle_upload_pdf(file: UploadFile):
+  if not file.filename.endswith(".pdf"):
+    raise HTTPException(status_code=400, detail={"error": "Bad Request", "message": "Only PDF files are allowed"})
+  
+  # Create file path for the uploaded file
+  file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+  # Save the uploaded file by reading it in chunks and writing it to the file path
+  with open(file_path, "wb") as buffer:
+    buffer.write(await file.read())
+
+  # Load and extract text using PyPDFLoader
+  loader = PyPDFLoader(file_path)
+  pages = await loader.aload()
+
+  return pages
