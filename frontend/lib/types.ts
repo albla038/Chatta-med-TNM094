d18b0e9defa980978c6c5edacdd630b0f633579f
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-04-16 15:46:23 +0200

    Changed messages from array to map data structure & JSON serializer functions

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -8,32 +8,11 @@ export type Message = {
 export type Conversation = {
   id: string;
   createdAt: number;
-  messages: Message[];
+  messages: Map<string, Message>;
   sentFirstMessage: boolean; // Flag to check if the first message has been sent
 };
 
-// TODO Types for sending JSON over WS
 export type WebSocketMessage =
-  // | { type: "userMessage"; content: string }
   | { type: "messageChunk"; id: string; content: string }
-  | { type: "done"; id: string }
-  | { type: "error"; error: string; details: string };
-
-// type ResponseMessage = {
-//   status: string;
-//   id: string;
-//   role: string;
-//   content: string;
-//   responseMetadata: null | {
-//     [key: string]: string;
-//   };
-// };
-
-// type ConversationItem = {
-//   messageId: string;
-//   role: string;
-//   content: string;
-//   metadata?: {
-//     [key: string]: string;
-//   };
-// };
+  | { type: "done"; id: string; content: string }
+  | { type: "error"; error: string; details: string }
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -5,19 +5,41 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getFromLocalStorage<T>(key: string): T | null {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function conversationReviver(key: string, value: any) {
+  if (value && value.__type === "Map") {
+    return new Map(value.value);
+  }
+  return value;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function conversationReplacer(key: string, value: any) {
+  if (value instanceof Map) {
+    return {
+      __type: "Map",
+      value: [...value.entries()],
+    };
+  }
+  return value;
+}
+
+export function getConversationFromLocalStorage<T>(key: string): T | null {
   try {
     const jsonData = localStorage.getItem(key);
-    return jsonData ? JSON.parse(jsonData) : null;
+    return jsonData ? JSON.parse(jsonData, conversationReviver) : null;
   } catch (error) {
     console.error(`Could not get localStorage item with key: ${key}`, error);
     return null;
   }
 }
 
-export function setToLocalStorage<T>(key: string, value: T): boolean {
+export function setConversationToLocalStorage<T>(
+  key: string,
+  value: T
+): boolean {
   try {
-    localStorage.setItem(key, JSON.stringify(value));
+    localStorage.setItem(key, JSON.stringify(value, conversationReplacer));
     return true;
   } catch (error) {
     console.error(`Could not set localStorage item with key: ${key}`, error);
