096151be3f3e1325261e87b9ffabdb9878b6f9c6
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-04-19 19:56:56 +0200

    Sidebar displays user info  & logout button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,20 @@
+import { auth } from "@/auth";
 import ChatLauncher from "@/components/chat-launcher";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  const user = session?.user;
+
   return (
     <>
       <div className="h-full w-full">
-        <ChatLauncher />
+        {user?.name ? (
+          <ChatLauncher />
+        ) : (
+          <div className="flex items-center justify-center h-full">
+            <p>Välkommen. Vänligen logga in</p>
+          </div>
+        )}
       </div>
     </>
   );
