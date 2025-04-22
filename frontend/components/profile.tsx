e4f65edce4f425d1ea8185f70e99904970631b0b
Albert Lännerström <albert.lannerstrom@gmail.com>
2025-04-22 12:21:50 +0200

    Added cursor pointer to Profile component

--- frontend/components/profile.tsx
+++ frontend/components/profile.tsx
@@ -1,10 +1,19 @@
+import { cn } from "@/lib/utils";
 import { Button } from "./ui/button";
 
-export function Profile({ initials }: { initials: string }) {
+type ProfileProps = {
+  className?: string;
+  initials: string;
+};
+
+export function Profile({ className, initials }: ProfileProps) {
   return (
     <Button
       size={"icon"}
-      className="focus:none rounded-full flex justify-center items-center bg-white stroke-1 border border-gray-300 shadow-none text-black font-light text-xl size-12 hover:bg-white"
+      className={cn(
+        className,
+        "focus:none rounded-full flex justify-center items-center bg-white stroke-1 border border-gray-300 shadow-none text-black font-light text-xl size-12 hover:bg-white"
+      )}
     >
       {initials}
     </Button>
